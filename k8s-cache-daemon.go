66f2e31b3caa595897a4c9458ad8c19e0abf9463
ilcavero <ilcavero@gmail.com>
2019-03-10 18:54:12 +0000

    attempting to watch a directory for files

--- k8s-cache-daemon.go
+++ k8s-cache-daemon.go
@@ -1,12 +1,12 @@
-package k8scachedaemon
+package main
 
 import (
 	"flag"
 	"log"
-	"os"
-	"path/filepath"
+	"strings"
 
 	"github.com/haxii/daemon"
+	"github.com/rjeczalik/notify"
 	av1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	nv1 "k8s.io/api/networking/v1"
@@ -16,25 +16,38 @@ import (
 )
 
 var (
-	kubeconfig = flag.String("kubeconfig", filepath.Join(os.Getenv("HOME"), ".kube", "config"), "absolute path to the kubeconfig file")
-	_          = flag.String("s", daemon.UsageDefaultName, daemon.UsageMessage)
+	configDir = flag.String("configDir", "", "absolute path to the dir containing configurations")
+	_         = flag.String("s", daemon.UsageDefaultName, daemon.UsageMessage)
 )
 
-func run() {
+func main() {
 	flag.Parse()
+	dirWatcher := make(chan notify.EventInfo, 100)
+	if err := notify.Watch(*configDir, dirWatcher, notify.Create); err != nil {
+		log.Fatal(err)
+	}
+	for {
+		e := <-dirWatcher
+		if strings.HasSuffix(e.Path(), "kubeconfig") {
+			log.Printf("new file in %s", e.Path())
+		} else {
+			log.Printf("NOT new file in %s", e.Path())
+		}
+	}
+}
+
+func watch(kubeconfig *string) {
 
 	// use the current context in kubeconfig
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		log.Fatal(err)
-		panic(err.Error())
 	}
 
 	// create the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		log.Fatal(err)
-		panic(err.Error())
 	}
 	pods, _ := clientset.CoreV1().Pods("").Watch(metav1.ListOptions{})
 	netpol, _ := clientset.NetworkingV1().NetworkPolicies("").Watch(metav1.ListOptions{})
@@ -69,6 +82,7 @@ func run() {
 
 }
 
+/*
 func main() {
 	daemon.Make("-s", "k8s-cache-daemon", "k8s cache daemon").Run(run)
-}
+}*/
